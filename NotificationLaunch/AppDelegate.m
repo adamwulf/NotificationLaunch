501ec57baa318ac82b98fe989fc89ede21fa1c59
Adam Wulf <adam.wulf@gmail.com>
2018-04-02 17:42:17 -0500

    initial commit

--- NotificationLaunch/AppDelegate.m
+++ NotificationLaunch/AppDelegate.m
@@ -7,8 +7,9 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
-@interface AppDelegate ()
+@interface AppDelegate ()<NSUserNotificationCenterDelegate>
 
 @end
 
@@ -16,6 +17,9 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    [[NSUserNotificationCenter defaultUserNotificationCenter] setDelegate:self];
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"ApplicationDidFinishLaunching" object:nil userInfo:@{ @"notification" : aNotification }];
 }
 
 
@@ -23,5 +27,22 @@
     // Insert code here to tear down your application
 }
 
+
+#pragma mark - NSUserNotificationCenter Delegate
+
+- (void)userNotificationCenter:(NSUserNotificationCenter *)center didActivateNotification:(NSUserNotification *)notification
+{
+    NSLog(@"Activated notification: %@", notification);
+}
+
+- (void)userNotificationCenter:(NSUserNotificationCenter *)center didDeliverNotification:(NSUserNotification *)notification
+{
+    NSLog(@"Delivered notification: %@", notification);
+}
+
+- (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center shouldPresentNotification:(NSUserNotification *)notification
+{
+    return YES;
+}
 
 @end
--- NotificationLaunch/ViewController.m
+++ NotificationLaunch/ViewController.m
@@ -7,21 +7,84 @@
 //
 
 #import "ViewController.h"
+#import "AppDelegate.h"
 
-@implementation ViewController
+@interface ViewController ()
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+@property (nonatomic, strong) IBOutlet NSTextView *textView;
 
-    // Do any additional setup after loading the view.
+@end
+
+@implementation ViewController{
+    NSNotification *_notification;
 }
 
-
-- (void)setRepresentedObject:(id)representedObject {
-    [super setRepresentedObject:representedObject];
-
-    // Update the view, if already loaded.
+-(void) updateTextView{
+    [_textView setString:[NSString stringWithFormat:@"notification user info: %@", [_notification userInfo]]];
 }
 
+-(void) viewDidLoad{
+    [[NSNotificationCenter defaultCenter] addObserver:self  selector:@selector(applicationDidFinishLaunching:) name:@"ApplicationDidFinishLaunching" object:nil];
+}
+
+-(void) applicationDidFinishLaunching:(NSNotification*)notification{
+    _notification = [notification userInfo][@"notification"];
+    [self updateTextView];
+}
+
+-(void) viewDidAppear{
+    [super viewDidAppear];
+    
+    [self updateTextView];
+}
+
+-(IBAction)triggerNotificationWithOptions:(id)sender{
+    NSUserNotification *userNotification = [[NSUserNotification alloc] init];
+    NSString *title = @"Additional Options";
+    NSString *informativeText = @"Missing user notification in launch notification";
+    
+    [userNotification setIdentifier:[[NSUUID UUID] UUIDString]];
+    [userNotification setTitle:title];
+    [userNotification setSubtitle:@"Subtitle"];
+    [userNotification setInformativeText:informativeText];
+    [userNotification setHasActionButton:YES];
+    [userNotification setActionButtonTitle:NSLocalizedString(@"Accept", @"Accept (accept an invitation)")];
+
+    NSArray *additionalActions = @[
+                                   [NSUserNotificationAction actionWithIdentifier:@"decline" title:NSLocalizedString(@"Decline", @"Decline (decline an invitation)")],
+                                   [NSUserNotificationAction actionWithIdentifier:@"maybe" title:NSLocalizedString(@"Maybe", @"Maybe (respond maybe to an invitation)")],
+                                   ];
+    
+    [userNotification setAdditionalActions:additionalActions];
+
+    [userNotification setOtherButtonTitle:NSLocalizedString(@"Close", @"Alarm notification")];
+
+    [[NSApplication sharedApplication] hide:nil];
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:userNotification];
+    });
+}
+
+-(IBAction)triggerNotificationWithoutOptions:(id)sender{
+    NSUserNotification *userNotification = [[NSUserNotification alloc] init];
+    NSString *title = @"No Additional Options";
+    NSString *informativeText = @"Shows user notification in launch notification";
+    
+    [userNotification setIdentifier:[[NSUUID UUID] UUIDString]];
+    [userNotification setTitle:title];
+    [userNotification setSubtitle:@"Subtitle"];
+    [userNotification setInformativeText:informativeText];
+    [userNotification setHasActionButton:YES];
+    [userNotification setActionButtonTitle:NSLocalizedString(@"Accept", @"Accept (accept an invitation)")];
+    
+    [userNotification setOtherButtonTitle:NSLocalizedString(@"Close", @"Alarm notification")];
+    
+    [[NSApplication sharedApplication] hide:nil];
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:userNotification];
+    });
+}
 
 @end
